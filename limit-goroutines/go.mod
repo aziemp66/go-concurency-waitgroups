c5da373598ff05200188cef057cee62806849328
Azie Melza Pratama <aziemp66@gmail.com>
2024-04-21 23:33:46 +0700

    feat : Rate Limiting Using WaitGroup

---
+++ limit-goroutines/go.mod
@@ -0,0 +1,3 @@
+module limit-goroutines
+
+go 1.22.2
---
+++ limit-goroutines/server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	var connections int32
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			continue
+		}
+		connections++
+
+		go func() {
+			defer func() {
+				conn.Close()
+				atomic.AddInt32(&connections, -1)
+			}()
+			if atomic.LoadInt32(&connections) > 3 {
+				return
+			}
+			time.Sleep(time.Second)
+			_, err := conn.Write([]byte("success"))
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+}
